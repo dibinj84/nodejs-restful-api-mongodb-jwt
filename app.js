bbaa2bd5c30ee6439fba78937db50bde8a068e58
Dibin Davis <dibinj84@gmail.com>
2019-04-09 14:07:56 +0400

    app.js updated

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+/*
 var UserController = require('./user/UserController');
 app.use('/users', UserController );
+*/
 
 var AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController );
